1c5a312b8030a925c230626462fdaef234d2098f
cuardin <daniel@armyr.se>
2014-08-24 09:20:58 +0200

    Site Seed calculation now works.

--- test/mpw.js
+++ test/mpw.js
@@ -10,8 +10,8 @@ function mpw_core_calculate_master_key_salt( mpNameSpace, userName )
 {	
 	//Convert strings to byte buffers
     var mpNameSpaceRaw = TextEncoder("utf-8").encode(mpNameSpace);
-	var userNameRaw = TextEncoder("utf-8").encode(userName);
-    	
+    var userNameRaw = TextEncoder("utf-8").encode(userName);
+
     //Allocate memory
     var salt     = new Uint8Array(mpNameSpaceRaw.length + 4/*sizeof(uint32)*/ + userNameRaw.length);
     var saltView = new DataView(salt.buffer);
@@ -22,4 +22,22 @@ function mpw_core_calculate_master_key_salt( mpNameSpace, userName )
     saltView.setUint32(i, userNameRaw.length, false/*big-endian*/); i += 4/*sizeof(uint32)*/;
     salt.set(userNameRaw, i); i += name.length;
     return salt;
+}
+
+function mpw_core_calculate_site_seed( mpNameSpace, siteName, siteCounter )
+{
+    //Convert strings to byte buffers
+    var mpNameSpaceRaw = TextEncoder("utf-8").encode(mpNameSpace);
+    var siteNameRaw = TextEncoder("utf-8").encode(siteName);
+
+    var data     = new Uint8Array(mpNameSpaceRaw.length + 4/*sizeof(uint32)*/ + siteNameRaw.length + 4/*sizeof(uint32)*/);
+    var dataView = new DataView(data.buffer);
+    var i = 0;
+
+    data.set(mpNameSpaceRaw, i); i += mpNameSpaceRaw.length;
+    dataView.setUint32(i, siteNameRaw.length, false/*big-endian*/); i += 4/*sizeof(uint32)*/;
+    data.set(siteNameRaw, i); i += siteNameRaw.length;
+    dataView.setUint32(i, siteCounter, false/*big-endian*/); i += 4/*sizeof(uint32)*/;  
+
+    return data;
 }
--- test/tests.js
+++ test/tests.js
@@ -1,15 +1,31 @@
 
+QUnit.test( "testConvertToHex", function( assert ) {
+	var rawBytes = [3, 156, 54];	
+	var bytes = new Uint8Array(rawBytes);		
+	var hexString = convertBufferToHex( bytes );
+	assert.equal( "039c36", hexString );
+});
+
 QUnit.test( "testMainSalt", function( assert ) {
 	var mpNameSpace = "com.lyndir.masterpassword";
 	var userName = "user01åäö";
 	var masterKeySalt = mpw_core_calculate_master_key_salt(mpNameSpace, userName)
   	var stringSalt = convertBufferToHex(masterKeySalt);
   	assert.equal( stringSalt, "636f6d2e6c796e6469722e6d617374657270617373776f72640000000c757365723031c3a5c3a4c3b6" );
+  	assert.equal( 12, TextEncoder("utf-8").encode(userName).length ); //Ensure utf8 encoding(So last 3 are 2-byte);
+    assert.equal( 41, masterKeySalt.length );
+
 });
 
-QUnit.test( "testConvertToHex", function( assert ) {
-	var rawBytes = [3, 156, 54];	
-	var bytes = new Uint8Array(rawBytes);		
-	var hexString = convertBufferToHex( bytes );
-	assert.equal( "039c36", hexString );
+QUnit.test( "testSiteSalt", function( assert ) {
+	var siteName = "site01.åäö";
+    var siteCounter = 3;
+    var mpNameSpace = "com.lyndir.masterpassword";
+
+	var siteSeed = mpw_core_calculate_site_seed( mpNameSpace, siteName, siteCounter );
+  	var stringSalt = convertBufferToHex(siteSeed);
+  	
+  	assert.equal( 13, TextEncoder("utf-8").encode(siteName).length ); //Ensure utf8 encoding(So last 3 are 2-byte);
+  	assert.equal( 46, siteSeed.length );
+  	assert.equal( stringSalt, "636f6d2e6c796e6469722e6d617374657270617373776f72640000000d7369746530312ec3a5c3a4c3b600000003" );
 });
