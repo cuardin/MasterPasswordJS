4ca2ab5cba59161bdae61511db0b6528df8cd7c4
cuardin <daniel@armyr.se>
2014-09-09 21:27:01 +0200

    More code cleaning.

--- js/integration.js
+++ js/integration.js
@@ -1,79 +1,32 @@
-//Get a list of all elements tagged with class "siteInput".
-inputList = document.getElementsByClassName("siteInput");
-//Add an eventhandler to all of these elements.
-for ( var i = 0; i < inputList.length; i++ ) {
-	inputList[i].addEventListener("input", startSiteWorker);
-    inputList[i].addEventListener("change", startSiteWorker);
-}
-
-//Get a list of all elements tagged with class "mainInput".
-inputList = document.getElementsByClassName("mainInput");
-//Add an eventhandler to all of these elements.
-for ( var i = 0; i < inputList.length; i++ ) {
-	inputList[i].addEventListener("input", onMainInputChange);
-}
-
-function onMainInputChange() {
-    document.getElementById("sitePassword").value = "";
-    enableComputeBtn();
-    
-    inputList = document.getElementsByClassName("siteInput");
-    //Add an eventhandler to all of these elements.
-    for ( var i = 0; i < inputList.length; i++ ) {
-        inputList[i].disabled = true;
-    }
-}
-
-//Add an event listener to check that number is properly entered.
-document.getElementById("siteCounter").addEventListener( "input", onInputNumberChange );
-
-function onInputNumberChange() {
-    var siteCounter = document.getElementById("siteCounter")
-    var value = parseInt(siteCounter.value);
-    if ( isNaN(value) ) {
-        siteCounter.style="box-shadow: rgba(256,0,0, 0.5) 0px 0px 8px; -moz-box-shadow: rgba(256,0,0, 0.5) 0px 0px 8px; -webkit-box-shadow: rgba(256,0,0, 0.5) 0px 0px 8px;";
-    } else {
-        siteCounter.style="";
-    }
-}
-
+//***************************************
+// Declare all globals.
 var userName = null;
 var masterPassword = null;
-
-var computeBtn = document.getElementById("compute");
-computeBtn.addEventListener("click", startMainWorker );
-
 var w = new Worker("../js/mpw_worker.js");
-if(typeof(w) == "undefined") {    	    	
-    document.getElementById("sitePassword").value = "Sorry, your browser does not support Web Workers...";
-    lockUI();
+
+//********************************************
+//Add event handlers to the worker object
+if(typeof(w) == "undefined") { //TODO: Separate out this check and run this first of all.
+    document.getElementById("mainDiv").innerHTML = "Sorry, your browser does not support Web Workers...";        
 } else {
     w.addEventListener( "message", workerEventHandler, false);
     w.addEventListener("error", workerEventHandler, false);
 }
 
-function workerEventHandler(event) {
-    //console.log( event );    
-    var data = JSON.parse(event.data);
-    //console.log( data );
+function workerEventHandler(event) {    
+    var data = JSON.parse(event.data);    
         
     //Add a delay to make sure we allways see an effect.
-    if ( data.type == "password" ) {           
-        setTimeout(function(event) {
-            document.getElementById("sitePassword").value = data.data;  
-            document.getElementById("progress").src = "blank.gif";
-            document.getElementById('compute').innerHTML = "Compute";
-            //document.getElementById("sitePassword").select(); //Select the password for copying           
-        }, 100);
+    if ( data.type == "password" ) {                   
+        document.getElementById("sitePassword").value = data.data;  
+        document.getElementById("progress").src = "blank.gif";
+        document.getElementById('compute').innerHTML = "Compute";                    
     } else if ( data.type == "mainKey" ) {
-        setTimeout(function(event) {            
-            document.getElementById("progress").src = "blank.gif";
-            document.getElementById('compute').innerHTML = "Compute";            
-            unlockUI();   
-            disableComputeBtn();
-            startSiteWorker();
-        }, 100);
-        
+        document.getElementById("progress").src = "blank.gif";
+        document.getElementById('compute').innerHTML = "Compute";            
+        unlockUI();   
+        disableComputeBtn();
+        startSiteWorker();                
     } else if ( data.type == "progress" ) {
         document.getElementById("compute").innerHTML = "Computing: " + data.data;
     } else {
@@ -81,6 +34,16 @@ function workerEventHandler(event) {
     }
     
 };
+
+
+//******************************
+//Make all siteInput elements recompute the site password.
+inputList = document.getElementsByClassName("siteInput");
+for ( var i = 0; i < inputList.length; i++ ) {
+	inputList[i].addEventListener("input", startSiteWorker);
+    inputList[i].addEventListener("change", startSiteWorker);
+}
+
 function startSiteWorker() {
     if(typeof(Worker) !== "undefined") {    	    	        
         
@@ -102,11 +65,44 @@ function startSiteWorker() {
         w.postMessage(jsonString);                      
 
         var img = document.getElementById("progress");
-        img.src = "ajax-loader.gif";               
-        
-        //document.getElementById('compute').innerHTML = "Computing";
+        img.src = "ajax-loader.gif";                              
     }
 }
+
+//******************************
+// Make all mainInput elements invalidate the password and reenable the compute button.
+inputList = document.getElementsByClassName("mainInput");
+for ( var i = 0; i < inputList.length; i++ ) {
+	inputList[i].addEventListener("input", onMainInputChange);
+}
+
+function onMainInputChange() {
+    document.getElementById("sitePassword").value = "";
+    enableComputeBtn();
+    
+    inputList = document.getElementsByClassName("siteInput");    
+    //Add an eventhandler to all of these elements.
+    for ( var i = 0; i < inputList.length; i++ ) {
+        inputList[i].disabled = true;
+    }    
+}
+
+//***************************************************************
+//Add an event listener to check that number is properly entered.
+document.getElementById("siteCounter").addEventListener( "input", onInputNumberChange );
+
+function onInputNumberChange() {
+    var siteCounter = document.getElementById("siteCounter")
+    var value = parseInt(siteCounter.value);
+    if ( isNaN(value) ) {
+        siteCounter.style="box-shadow: rgba(256,0,0, 0.5) 0px 0px 8px; -moz-box-shadow: rgba(256,0,0, 0.5) 0px 0px 8px; -webkit-box-shadow: rgba(256,0,0, 0.5) 0px 0px 8px;";
+    } else {
+        siteCounter.style="";
+    }
+}
+
+document.getElementById("compute").addEventListener("click", startMainWorker );
+
 function startMainWorker() {
     if(typeof(Worker) !== "undefined") {    	    	
         lockUI();        
