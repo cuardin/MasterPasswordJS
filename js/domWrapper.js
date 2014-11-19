d79de49caf93d81cb77dfb40c2d3f150e0d5db1f
cuardin <daniel@armyr.se>
2014-11-19 19:45:51 +0100

    Cleaned up the code (Unfortunately, I haven't been able to test it all).

--- js/domWrapper.js
+++ js/domWrapper.js
@@ -21,6 +21,18 @@ function getAllInputsFromForm(  ) {
     data.siteType = $('#siteType').val();    
     data.capchaResponse = $("#recaptcha_response_field").val();
     data.capchaChallenge = $("#recaptcha_challenge_field").val();
+   
+    //Now make sure to cap all inputs.
+    keys = Object.keys(data);
+    for ( var i = 0; i < keys.length; i++ ) {                
+        if ( typeof(data[keys[i]]) ===  "string" && data[keys[i]].length > 1024 ) {
+            //First truncate the value we read.
+            data[keys[i]] = data[keys[i]].substr(0,1024);
+            //Then make sure that the input field is truncated as well.
+            $("#" + keys[i]).val(data[keys[i]]);
+            popupDialog( "Truncated input", "The value of " + keys[i] + " was too long. It has been truncated to 1024 characters." );
+        }
+    }
     return data;
 }
 
