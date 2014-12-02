5eaba2f87bcffcbac0c03e405bfbd129400f757b
cuardin <daniel@armyr.se>
2014-12-02 15:27:18 +0100

    Authentication is now done in constant time.

--- database/core/utilities.php
+++ database/core/utilities.php
@@ -32,29 +32,69 @@ function connectDatabase() {
     return $mysql;
 }
 
+/**
+* Verify a password against a hash using a timing attack resistant approach
+* @return boolean If the password matches the hash
+*/
+function password_verify($password, $passwordStored) {
+          
+   $status = 0;
+   for ($i = 0; $i < strlen($password); $i++) {
+       $status |= (ord($password[$i]) ^ ord($passwordStored[$i]));
+   }
+
+   return $status === 0;
+}
+
 function authenticateUser($mysql, $username, $password) {
 
-    //First check if the user name exists
-    //This introduces a security concern in that the list of user names can 
-    //be leaked, but I do not care.
-    $usernameStored = getOneValueFromUserList($mysql, "username", $username);
-    if ( strcmp( $username, $usernameStored) ) {
-        throw new Exception ( "BAD_LOGIN" );
-    }
-    
-    //Then get the password. This setup allows a hacker to do a timing attach 
-    //and find the list of usernames in the database. Ignoring that for now.
-    $passwordStored = getOneValueFromUserList($mysql, "password", $username);  
-    
+    //First get the password.    
+    $passwordStored = getPasswordFromDatabase($mysql, $username);    
+            
     //Hash the password, using the salt stored
     $passwordCrypt = crypt($password, $passwordStored);                         
     
+    error_log ( "$password" );
+    error_log ( "$passwordStored" );
+    error_log ( "$passwordCrypt" );
+    
     //Now check the fetched password against the stored
-    if (strcmp($passwordStored, $passwordCrypt)) {                
+    if (!password_verify($passwordStored, $passwordCrypt)) {                
         throw new Exception( "BAD_LOGIN" );
     }    
     
     return true;
+}
+
+function getPasswordFromDatabase($mysql, $username) {
+    $query = 'SELECT password FROM masterpassword_users WHERE username=?';
+    $stmt = $mysql->prepare($query);
+    $value = ''; //Initialize
+    if (!$stmt) {
+        throw new Exception ( "SQL Syntax Error");
+    }   
+    if ( !$stmt->bind_param('s', $username) ) {
+        throw new Exception ( "Error binding parameter");
+    }
+    if ( !$stmt->execute() ) {
+        throw new Exception( "Error executing SQL statement");
+    }
+    if ( !$stmt->bind_result($value) ) {
+        throw new Exception ( "Error binding result");
+    }    
+    if ( $stmt->fetch() === false ) {
+        throw new Exception ( "Error fetching data" );
+    }    
+    if ( !$stmt->close() ) {
+        throw new Exception( "Error closing statemebt");
+    }
+    
+    if ( $value == '' ) {
+        $value = $value."$6$000000000000$00000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+    } else {
+        $value = ''.$value;
+    }
+    return $value;
 }
 
 function getUserNameFromEmail($mysql, $email) {
